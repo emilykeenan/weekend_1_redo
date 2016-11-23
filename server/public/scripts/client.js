9775ff3942b1f38434ce7d73d474f7c9e13be164
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-23 17:20:25 -0600

    toggle active working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -21,21 +21,36 @@ app.controller("EmployeesController", ["$http", function($http){
   getEmployees();
 
   function getEmployees() {
-
-
     $http.get('/employees')
     .then(function(response) {
       self.employees = response.data;
+      self.avgSalaryOutput = 0;
       for (var i = 0; i < self.employees.length; i++) {
         var employee = self.employees[i];
         if (employee.status=='active') {
         self.avgSalaryOutput += (employee.yearly_salary / 12);
         }
       }
-      self.avgSalaryOutput.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+      self.avgSalaryOutput = self.avgSalaryOutput.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
     });
+  };  // get Employees function ends
 
-  }; // get Employees function ends
+  self.updateEmployee = function(employee) {
+    console.log(employee);
+    var id = employee.id
+    if(employee.status == 'active') {
+      employee.status = 'inactive';
+      console.log(employee.status);
+    } else if(employee.status == 'inactive') {
+      employee.status = 'active';
+      console.log(employee.status);
+    }
+
+    $http.put('/employees/' + id, employee)
+    .then(function(response) {
+      getEmployees();
+    });
+  };  // updateEmployee function ends
 
 
 
--- server/routes/employees.js
+++ server/routes/employees.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res) {
       res.sendStatus(500);
     }
 
-    client.query('SELECT * FROM employees;',
+    client.query('SELECT * FROM employees ORDER BY last_name;',
     function(err, result) {
       done(); // close the connection.
 
@@ -20,12 +20,43 @@ router.get('/', function(req, res) {
         console.log('select query error: ', err);
         res.sendStatus(500);
       }
-      console.log(result.rows);
+      // console.log(result.rows);
       res.send(result.rows);
 
     });
 
   });
 });
+
+router.put('/:id', function(req, res) {
+  console.log(req.body);
+  employeeID = req.params.id;
+  employee = req.body;
+  employeeStatus = req.body.status;
+  console.log('employee to update ', employeeStatus);
+
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query(
+      'UPDATE employees SET status=$1 ' +
+      'WHERE id=$2',
+      // array of values to use in the query above
+      [employee.status, employeeID],
+      function(err, result) {
+        done();
+        if(err) {
+          console.log('update error: ', err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      });
+    }); // close connect
+
+}); // end route
 
 module.exports = router;
